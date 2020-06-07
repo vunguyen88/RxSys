a8a931ec2e1ab5dc25dd9388655d8f2848030127
unknown <nguyenanhhoanvu@gmail.com>
2020-06-07 19:00:09 -0400

    update name

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,11 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
+var moment = require('moment');
+moment().format(); 
+
 const { 
-    getAllPatients, 
+    getAllPatients,
     getPatientsinPharmacy, 
     createPatient, 
     findPatient, 
@@ -19,7 +22,10 @@ const {
     createPharmacy, 
     signup, 
     login, 
-    addPharmacyDetails 
+    addPharmacyDetails,
+    resetPassword,
+    signOut,
+    changePassword 
 } = require('./handlers/pharmacies');
 
 const { 
@@ -27,32 +33,48 @@ const {
     getMedList, 
     addMed,
     getPatientMedList,
-    deleteMedication 
+    deleteMedication,
+    //mobileMedUpdate 
 } = require('./handlers/medications');
 
-// Patients route
-app.get('/patients', getAllPatients);
-app.get('/getPatientsinPharmacy', getPatientsinPharmacy); // function to get all the patients inside pharmacy collection
-app.get('/patient/:patientId', FBAuth, getPatient);
-app.post('/createPatient', FBAuth, createPatient); //Checked
-app.put('/patient/:patientId', FBAuth, updatePatientInfo); //Checked
-app.post('/findPatient', findPatient);
-app.delete('/patient/:patientId', FBAuth, deletePatient); //Checked
+const {
+    mobileMedUpdate,
+    getTodayMedList,
+    getAllPatientsMobile,
+} = require('./handlers/mobile');
 
 // Pharmacy route
-app.get('/pharmacy', getAllpharmacies);
+app.get('/signout', FBAuth, signOut);
+app.put('/password', FBAuth, changePassword);
+app.post('/pharmacists/:email', resetPassword);
+app.get('/pharmacy', FBAuth, getAllpharmacies); // Working
 app.post('/createPharmacy', createPharmacy);
-app.post('/signup', signup); //Checked
-app.post('/login', login); //Checked
-app.put('/pharmacy/:pharmacyName', FBAuth, addPharmacyDetails); //Checked
-app.delete('/patient/:patientId/medication')
+app.post('/signup', signup); // Working
+app.post('/login', login); // Working
+app.put('/pharmacists/:pharmacist', FBAuth, addPharmacyDetails); // Working
+app.delete('/patients/:patient/medication'); 
+
+// Patients route
+app.get('/patients', FBAuth, getAllPatients); // Working
+app.get('/getPatientsinPharmacy', getPatientsinPharmacy); // No longer used //function to get all the patients inside pharmacy collection
+app.get('/patients/:patient', FBAuth, getPatient); // Working
+app.post('/patients', FBAuth, createPatient); // Checked
+app.put('/patients/:patient', FBAuth, updatePatientInfo); // Working
+app.get('/patients/search/:patient', findPatient); // Need fix
+app.delete('/patients/:patient', FBAuth, deletePatient); //Working
 
 // Medication route
-app.post('/patient/:patientId/addMedication', FBAuth, addMedication); // no longer used
-app.post('/patient/:patientId/medication', FBAuth, addMed); //added new medication into medications collection //Checked
-app.get('/patient/:patientId/medication', FBAuth, getPatientMedList); //Checked
-app.get('/getMedList', FBAuth, getMedList); // get all meds in medications collection //Checked
-app.delete('/patient/:patientId/medication/:medId', FBAuth, deleteMedication);
+app.post('/patient/:patient/addMedication', FBAuth, addMedication); // no longer used
+app.post('/patient/:patient/medication', FBAuth, addMed); //Working
+app.get('/patient/:patient/medication', FBAuth, getPatientMedList); // Working but need update
+app.get('/getMedList', FBAuth, getMedList); // get all meds in medications collection // Working
+app.delete('/patient/:patient/medicationd/:medication', FBAuth, deleteMedication);
+
+// Mobile route
+app.post('/patients/:patient/medications/:medication/mobileMedUpdate', mobileMedUpdate); // send update to firebase whether patient taking med or not
+app.get('/patients/:patient/getTodayMedList', getTodayMedList);
+//app.get('/patient/:patientId/getPatientMedListMobile', getPatientMedListMobile);
+app.get('/patientsMobile', getAllPatientsMobile); // Working
 
 // https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
